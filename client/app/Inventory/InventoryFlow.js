720f7b86f725065765ea4abed79b5900dff2898d
ChadFong <ChadMFong@Gmail.com>
2015-03-09 12:49:06 -0700

    Adds auto-populating inventory template

--- client/app/Inventory/InventoryFlow.js
+++ client/app/Inventory/InventoryFlow.js
@@ -1,6 +1,13 @@
 angular.module('inventory', [])
 
 .controller('InventoryController', function($scope){
+  $scope.data = [{'product': 'Mandarins',
+                  'count': 400,
+                  'brand': 'Cuties'},
+                 {'product': 'Potatoes, Russet',
+                  'count': 800,
+                  'brand': 'Potatoes, Inc.'}
+                  ];
   console.log('loading Controller');
 });
 
---
+++ client/app/Services/Services.js
@@ -0,0 +1,32 @@
+// angular.module('inventory.services', [])
+
+// .factory('Links', function ($http) {
+//   //PlaceHolder
+
+
+//   // return {
+//   //   // PlaceHolder
+//   // };
+// });
+
+
+/*
+<div ng-app="" ng-controller="customersController"> 
+
+<table>
+  <tr ng-repeat="x in names">
+    <td>{{ x.Name }}</td>
+    <td>{{ x.Country }}</td>
+  </tr>
+</table>
+
+</div>
+
+<script>
+function customersController($scope,$http) {
+  $http.get("http://www.w3schools.com/website/Customers_JSON.php")
+  .success(function(response) {$scope.names = response;});
+}
+</script>
+
+*/
