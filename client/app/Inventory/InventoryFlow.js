69d83593966f3b4746b4f9901e1aee36a13b26ee
ChadFong <ChadMFong@Gmail.com>
2015-03-10 15:00:13 -0700

    Incoming and Current Inventory display on page

--- client/app/Inventory/InventoryFlow.js
+++ client/app/Inventory/InventoryFlow.js
@@ -1,10 +1,10 @@
 angular.module('inventory', [])
 
 .controller('InventoryController', function($scope, $location, Inventory){
-  $scope.productData = {};
   $scope.data = [];
+  $scope.incoming = [];
+
   $scope.url = $location.$$url.slice(1);
-  $scope.transfer = {};
   $scope.options = [
     {label: 'Receiving', value: 'Receiving'},
     {label: 'Cold Storage', value: 'ColdStorage'},
@@ -12,6 +12,9 @@ angular.module('inventory', [])
     {label: 'Pack Out', value: 'PackOut'},
     {label: 'Kitchen', value: 'Kitchen'}
   ];
+  
+  $scope.productData = {};
+  $scope.transfer = {};
 
   $scope.addData = function(){
     Inventory.addProduct($scope.url ,
@@ -23,15 +26,16 @@ angular.module('inventory', [])
 
   $scope.getData = function() {
     $scope.data = Inventory.pullData($scope.url);
+    $scope.incoming = Inventory.pullData($scope.url + '_Incoming');
   };
 
   $scope.submitTransfer = function(){
     var destination = $scope.selection + '_Incoming';
     var transfers = $scope.transfer;
     for(var product in transfers){
-// Updates inventory count
+      // Updates inventory count
       Inventory.updateInventory($scope.url, transfers[product].$id, {'count': transfers[product].count - transfers[product].amount});
-// Adds outgoing product to destination's Incoming Area
+      // Adds outgoing product to destination's Incoming Area
       Inventory.transferInventory(destination, {
         'ID': transfers[product].$id,
         'product': transfers[product].product,
@@ -46,6 +50,13 @@ angular.module('inventory', [])
     $scope.transfer[index].active = !($scope.transfer[index].active);
     $scope.transfer[index].amount=null;
     console.log($scope.transfer[index]);
+  };
+
+  $scope.confirmTransfer = function(){
+    for(var i=0 ; i<incoming.length ; i++){
+      //ID is different, we need to be able to identify by product.
+      // Inventory.updateInventory($scope.url, );
+    }
   };
 
   $scope.getData();
